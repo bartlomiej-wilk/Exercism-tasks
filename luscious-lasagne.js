1824a0a83835cd9cf3d55b985df62880b3e87660
Bartlomiej Wilk <bartlomiej.wilk@interia.pl>
2022-06-05 00:37:23 +0200

    add task 2 - Lucian's Luscious Lasagna

---
+++ luscious-lasagne.js
@@ -0,0 +1,18 @@
+const PREPARATION_MINUTES_PER_LAYER = 2;
+const EXPECTED_MINUTES_IN_OVEN = 40;
+
+function remainingMinutesInOven(actualMinutesInOven) {
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+}
+
+function preparationTimeInMinutes(numberOfLayers) {
+  return PREPARATION_MINUTES_PER_LAYER * numberOfLayers;
+}
+
+function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+}
+
+console.log(remainingMinutesInOven(10));
+console.log(preparationTimeInMinutes(2));
+console.log(totalTimeInMinutes(2, 10));
